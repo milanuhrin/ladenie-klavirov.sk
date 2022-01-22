d0a20f6811aa6b0195c3bf3861b9afa9b22bf0db
Rastislav Uhrin <rastouhrin1@gmail.com>
2022-01-22 21:14:22 +0100

    added content to future lib

---
+++ src/Components/laterForLibrary/index.ts
@@ -0,0 +1,20 @@
+//nevymazat! priklad toho jak sa robi prepouzitelny styled comp s tw
+export const P = styled(motion.p)(
+   ({ textColor, gradient }: { textColor?: string; gradient?: string }) => [
+      `color: ${textColor};`,
+      gradient &&
+         `background: ${gradient};
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;`,
+      tw`text-xl`,
+   ]
+);
+
+export const H3 = styled(motion.h3)(({ gradient }: { gradient?: string }) => [
+   tw`self-center text-4xl font-extrabold`,
+   gradient &&
+      `background: ${gradient};
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;`,
+   // NEVYMAZAT -> format of code for gradiet:  '-webkit-linear-gradient(#eee, #333)'
+]);
