d3281603041301e6c864c06bf5ed96e57582e9df
rasto <rastouhrin1@gmail.com>
2022-02-09 19:13:11 +0100

    installed husky and lint-staged

--- src/Components/Navigation/Desktop/DesktopNav.tsx
+++ src/Components/Navigation/Desktop/DesktopNav.tsx
@@ -8,6 +8,7 @@ import { container, desktopMenuItems } from 'Utilities/motionVariants'
 interface Props {}
 
 export const DesktopNav = (props: Props) => {
+   const wat = 1
    return (
       <>
          <motion.ul
